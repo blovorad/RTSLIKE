ab73ad4e2ff9ed43429119a0c294a7a5efcdd381
girard <girard@Vince>
2021-02-16 16:05:07 +0100

    Modification de la classe Unit

--- src/engine/Unit.java
+++ src/engine/Unit.java
@@ -1,6 +1,86 @@
 package engine;
+/**
+ *
+ * création:16/02/2021
+ * @author Girard 
+ * @version:16/02/2021
+ * 
+ */
 
-public class Unit 
+public class Unit extends Entity
 {
+	private int currentAction;
+	private int attackRange;
+	private int damage;
+	private int range;
+	private int armor;
+	
+	private Speed speed;
+	
+	public Unit()
+	{
+		
+	}
+	
+	public int getDamage() 
+	{
+		return damage;
+	}
 
+	public void setDamage(int damage) 
+	{
+		this.damage = damage;
+	}
+
+	public int getRange() 
+	{
+		return range;
+	}
+
+	public void setRange(int range) 
+	{
+		this.range = range;
+	}
+
+	public int getArmor() 
+	{
+		return armor;
+	}
+
+	public void setArmor(int armor) 
+	{
+		this.armor = armor;
+	}
+
+	public int getAttackRange() 
+	{
+		return attackRange;
+	}
+
+	public void setAttackRange(int attackRange) 
+	{
+		this.attackRange = attackRange;
+	}
+
+	public int getCurrentAction() 
+	{
+		return  currentAction;
+	}
+
+	public void setCurrentAction(int currentAction) 
+	{
+		this.currentAction = currentAction;
+	}
+
+	public Speed getSpeed() 
+	{
+		return speed;
+	}
+
+	public void setSpeed(Speed speed) 
+	{
+		this.speed = speed;
+	}
+
+	
 }
