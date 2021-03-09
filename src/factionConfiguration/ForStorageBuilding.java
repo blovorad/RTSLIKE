795d27e8eb7c6648b811bd0fa057c64c0e78502b
maxime grodet <grodet95@gmail.com>
2021-03-09 16:01:32 +0100

    ressource storageBuilding

--- src/factionConfiguration/ForStorageBuilding.java
+++ src/factionConfiguration/ForStorageBuilding.java
@@ -2,8 +2,20 @@ package factionConfiguration;
 
 public class ForStorageBuilding extends Patron{
 	
+	private int ressources;
+	
 	public ForStorageBuilding(int hp, int age, String description, int hpMax)
 	{
 		super(hp, age, description, hpMax);
 	}
+
+	public int getRessources() {
+		return ressources;
+	}
+
+	public void setRessources(int ressources) {
+		this.ressources = ressources;
+	}
+	
+	
 }
